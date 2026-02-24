7e532df21d5fe1b9c32b2e16f2eafcba1a315b72
abdiflash <abdiflash@gmail.com>
2026-02-24 08:58:32 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    SCHOOL_NAME: "Template Renungan Harian",
+    SCHOOL_SITE: "edueasy.id",
+    SCHOOL_URL: "https://edueasy.id",
+
+    SHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY",
+    SHEET_NAME: "renungan",
+    SHEET_GID: "0",
+
+    COUNTER_NAMESPACE: "template-renungan",
+    COUNTER_KEY: "visitor_count"
+};
